d83f62e19f3b651ff7ebe0a77d4b251f0d8ccd65
Michal Špaček <tupinek@gmail.com>
2013-11-03 13:05:42 +0100

    Rewrite EXAMPLE2 in doc to use Data::Printer.

--- Czech.pm
+++ Czech.pm
@@ -86,7 +86,7 @@ See documentation for L<Acme::CPANAuthors> for more details.
 
  # Modules.
  use Acme::CPANAuthors;
- use Dumpvalue;
+ use Data::Printer;
 
  # Create object.
  my $authors = Acme::CPANAuthors->new('Czech');
@@ -94,31 +94,31 @@ See documentation for L<Acme::CPANAuthors> for more details.
  # Get all ids.
  my @ids = $authors->id;
 
- # Dump out.
- my $dump = Dumpvalue->new;
- $dump->dumpValues(\@ids);
+ # Print out.
+ p @ids;
 
  # Output:
- # 0  ARRAY(0x9c01980)
- #    0  'DANPEDER'
- #    1  'HIHIK'
- #    2  'HPA'
- #    3  'JANPAZ'
- #    4  'JANPOM'
- #    5  'JENDA'
- #    6  'JSPICAK'
- #    7  'KLE'
- #    8  'MAJLIS'
- #    9  'MILSO'
- #    10  'MJFO'
- #    11  'PAJAS'
- #    12  'PEK'
- #    13  'RUS'
- #    14  'RVASICEK'
- #    15  'SKIM'
- #    16  'TKR'
- #    17  'TRIPIE'
- #    18  'ZOUL'
+ # [
+ #     [0]  "DANPEDER",
+ #     [1]  "HIHIK",
+ #     [2]  "HPA",
+ #     [3]  "JANPAZ",
+ #     [4]  "JANPOM",
+ #     [5]  "JENDA",
+ #     [6]  "JSPICAK",
+ #     [7]  "KLE",
+ #     [8]  "MAJLIS",
+ #     [9]  "MILSO",
+ #     [10] "MJFO",
+ #     [11] "PAJAS",
+ #     [12] "PEK",
+ #     [13] "RUS",
+ #     [14] "RVASICEK",
+ #     [15] "SKIM",
+ #     [16] "TKR",
+ #     [17] "TRIPIE",
+ #     [18] "ZOUL"
+ # ]
 
 =head1 DEPENDENCIES
 
--- examples/ex2.pl
+++ examples/ex2.pl
@@ -6,7 +6,7 @@ use warnings;
 
 # Modules.
 use Acme::CPANAuthors;
-use Dumpvalue;
+use Data::Printer;
 
 # Create object.
 my $authors = Acme::CPANAuthors->new('Czech');
@@ -14,28 +14,28 @@ my $authors = Acme::CPANAuthors->new('Czech');
 # Get all ids.
 my @ids = $authors->id;
 
-# Dump out.
-my $dump = Dumpvalue->new;
-$dump->dumpValues(\@ids);
+# Print out.
+p @ids;
 
 # Output:
-# 0  ARRAY(0x9c01980)
-#    0  'DANPEDER'
-#    1  'HIHIK'
-#    2  'HPA'
-#    3  'JANPAZ'
-#    4  'JANPOM'
-#    5  'JENDA'
-#    6  'JSPICAK'
-#    7  'KLE'
-#    8  'MAJLIS'
-#    9  'MILSO'
-#    10  'MJFO'
-#    11  'PAJAS'
-#    12  'PEK'
-#    13  'RUS'
-#    14  'RVASICEK'
-#    15  'SKIM'
-#    16  'TKR'
-#    17  'TRIPIE'
-#    18  'ZOUL'
+# [
+#     [0]  "DANPEDER",
+#     [1]  "HIHIK",
+#     [2]  "HPA",
+#     [3]  "JANPAZ",
+#     [4]  "JANPOM",
+#     [5]  "JENDA",
+#     [6]  "JSPICAK",
+#     [7]  "KLE",
+#     [8]  "MAJLIS",
+#     [9]  "MILSO",
+#     [10] "MJFO",
+#     [11] "PAJAS",
+#     [12] "PEK",
+#     [13] "RUS",
+#     [14] "RVASICEK",
+#     [15] "SKIM",
+#     [16] "TKR",
+#     [17] "TRIPIE",
+#     [18] "ZOUL"
+# ]
