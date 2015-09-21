5398260caa38f837f2ee42833224d6a5b8c74346
Michal Špaček <tupinek@gmail.com>
2015-09-21 22:13:15 +0200

    Prepare for 0.20 version.

--- Czech.pm
+++ Czech.pm
@@ -6,7 +6,7 @@ use utf8;
 use warnings;
 
 # Version.
-our $VERSION = 0.19;
+our $VERSION = 0.20;
 
 # Modules.
 use Acme::CPANAuthors::Register(
@@ -174,6 +174,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.19
+0.20
 
 =cut
